0797cdd9ccfcac7a5188d5f39c50e59d5d9319b2
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-02-12 09:19:45 +0530

    commit 13

--- movies-frontend/src/App.jsx
+++ movies-frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import api from './api/axiosConfig';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
 
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/v1/movies');
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (error) {
+      console.error('Error fetching movies: ', error);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
     <>
